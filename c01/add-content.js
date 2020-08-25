d6c452b8eb91ede80ad429474407228d573ce3f8
Omar Zain <omaren2010@gmail.com>
2020-08-26 00:45:31 +0300

    New Interface

--- c01/add-content.js
+++ c01/add-content.js
@@ -3,11 +3,10 @@ var today= new Date();
  var hourNow = today.getHours();
   var greeting; 
 
-if (num > 18) {
-    greeting= 'Good evening!';}
- else if (num > 12) { greeting = ' Good afternoon!';}
-  else if (num > 0) { greeting = 'Good morning!';}
-   else if ( num > 24) { 
-      greeting = 'Welcome!' ; }
+if (num > 24) {greeting= 'Welcome!';}
+   else if ( num > 18) { greeting = 'Good evening!';}
+   else if (num > 12) { greeting = 'Good afternoon!';}
+   else if (num > 0) { greeting = 'Good morning!';}
+   
 
-document .write( ' <h3>' + greeting + ' </ h3>'); 
+document.write( '<h3>' + greeting + '</h3>'); 
